d6d4a8cb9886b227fce3f12ca315fcfb6a1a652f
Manthan <mksoul1811@gmail.com>
2024-12-30 01:48:00 +0530

    Fixed build folder

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: "export",
+	images: {
+		unoptimized: true, // Disables image optimization
+	},
 };
 
 export default nextConfig;
